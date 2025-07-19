c37d642f3842d1a3355046fa33c24b913d6bc681
Mohit Verma <156345473+Mohit12345789@users.noreply.github.com>
2025-07-19 17:29:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js on EKS!');
+  res.send('Hello from Node.js on EKS!..................');
 });
 
 app.listen(port, () => {
